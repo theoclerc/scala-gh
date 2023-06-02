d437f9ab9e6bdc9e4ba0aa1e58945c639fedd17e
Julienne Bétrisey <julienne.betrisey@students.hevs.ch>
2023-06-02 21:24:46 +0200

    Modify selection menu

--- src/main/scala/DataReader.scala
+++ src/main/scala/DataReader.scala
@@ -72,44 +72,47 @@ object DataExploration {
     // Close the reader
     reader.close()
 
-    var choice = 0
+    var choice = ""
 
     // Menu loop
-    while (choice != 5) {
+    while (choice != "Exit") {
       println("Menu:")
       println("1. Filter restaurants with more than 2 stars")
       println("2. Calculate the average menu price in Euros")
       println("3. Find the restaurant with the highest ranking")
       println("4. Group restaurants by country")
       println("5. Count the number of restaurants in each city")
-      println("Enter the number of the action you want to perform (1-5):")
-      choice = StdIn.readInt()
+      println("Exit. Exit the program")
+      println("Enter the number or 'Exit' to choose the action you want to perform:")
+      choice = StdIn.readLine()
 
       choice match {
-        case 1 =>
+        case "1" =>
           val highlyRatedRestaurants = restaurantData.filter(_.stars.exists(_ > 2))
           println("Highly rated restaurants:")
           highlyRatedRestaurants.foreach(println)
-        case 2 =>
+        case "2" =>
           val averagePrice = restaurantData.map(_.menu).sum.toDouble / restaurantData.length
           println(s"Average menu price in Euros: $averagePrice")
-        case 3 =>
+        case "3" =>
           val highestRankingRestaurant = restaurantData.maxBy(_.ranking)
           println("Restaurant with the highest ranking:")
           println(highestRankingRestaurant)
-        case 4 =>
+        case "4" =>
           val restaurantsByCountry = restaurantData.groupBy(_.country)
           println("Restaurants grouped by country:")
           restaurantsByCountry.foreach { case (country, restaurants) =>
             println(s"Country: $country")
             restaurants.foreach(println)
           }
-        case 5 =>
+        case "5" =>
           val restaurantCountByCity = restaurantData.groupBy(_.city).mapValues(_.size)
           println("Restaurant count by city:")
           restaurantCountByCity.foreach { case (city, count) =>
             println(s"$city: $count")
           }
+        case "Exit" => // Exit the program
+          println("Exiting the program...")
         case _ =>
           println("Invalid choice. Please enter a number between 1 and 5.")
       }
