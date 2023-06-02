f6005236b46b88ae07690bfbb5a47eaecea74311
Theoc <theo.clerc@students.hevs.ch>
2023-06-02 20:31:41 +0200

    Init DataReader

--- build.sbt
+++ build.sbt
@@ -1,1 +1,3 @@
 ThisBuild / scalaVersion := "3.1.3"
+
+libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.3.8"
---
+++ src/main/scala/DataReader.scala
@@ -0,0 +1,9 @@
+import com.github.tototoshi.csv._
+import java.io.File
+
+
+val reader = CSVReader.open(new File("data/02-50BestRestaurants.csv"))
+
+
+
+
